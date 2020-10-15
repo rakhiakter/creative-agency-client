9ecf5499d5ee7ea2d73c2c4577588af71722aa70
Rakhi Akter <67531354+rakhiakter@users.noreply.github.com>
2020-10-15 11:32:11 +0600

    code added

--- src/Components/Order/Order.js
+++ src/Components/Order/Order.js
@@ -1,10 +1,78 @@
 import React from 'react';
+import Sidebar from '../Sidebar/Sidebar';
+import './Order.css';
 
+
+const containerStyle = {
+  backgroundColor: "#F4FDFB",
+  height: "100%",
+};
 const Order = () => {
     return (
-        <div>
-         <h1>this is order page</h1>   
+      <section>
+        <div style={containerStyle} className="container-fluid row">
+          <div className="col-md-2">
+           
+            <Sidebar></Sidebar>
+          </div>
+          <div className="col-md-5 d-flex justify-content-center">
+            <form>
+              <div class="form-group">
+                <input
+                  type="text"
+                  class="form-control"
+                  id="formGroupExampleInput"
+                  placeholder="Your name/Company's name"
+                />
+              </div>
+              <div class="form-group">
+                <input
+                  type="email"
+                  class="form-control"
+                  id="formGroupExampleInput2"
+                  placeholder=" Your email address "
+                />
+              </div>
+              <div class="form-group">
+                <input
+                  type="text"
+                  class="form-control"
+                  id="formGroupExampleInput2"
+                  placeholder="Graphic design"
+                />
+              </div>
+              <div class="form-group">
+                <input
+                  type="text"
+                  class="form-control"
+                  id="formGroupExampleInput2"
+                  placeholder="Project details"
+                />
+              </div>
+              <div class="form-group">
+                <input
+                  type="text"
+                  class="form-control"
+                  id="formGroupExampleInput2"
+                  placeholder="Price"
+                />
+              </div>
+              <div className="form-group">
+                
+                <input
+                 
+                  type="file"
+                  className="form-control"
+                  id="exampleInputPassword1"
+                  placeholder="Picture"
+                />
+              </div>
+              <button className="order">Send</button>
+            </form>
+          </div>
+          <div className="col-md-5"></div>
         </div>
+      </section>
     );
 };
 
---
+++ src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Sidebar = () => {
+    return (
+      <div
+        className="sidebar d-flex flex-column justify-content-between col-md-2 py-5 px-4"
+        style={{ height: "100vh" }}
+      >
+        <ul>
+          <li>
+            {" "}
+            <Link to="/order">
+              <img
+                src={require("../../images/shopping-cart-outline 1.png")}
+                alt=""
+              />
+              <p>Order</p>
+            </Link>
+          </li>
+
+          <li>
+            <Link to="/serviceList">
+              <img
+                src={require("../../images/hard-drive-outline 1.png")}
+                alt=""
+              />
+              <p>Service list</p>
+            </Link>
+          </li>
+
+          <li>
+            <Link to="/review">
+              <img
+                src={require("../../images/review 1368.png")}
+                alt=""
+              />
+              <p>Review</p>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    );
+};
+
+export default Sidebar;
